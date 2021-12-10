17481c7f6c08714166083bc3cad6355a55c4c2b4
siddik <spolatde@gmail.com>
2021-12-10 21:07:42 +0100

    delete eigenschaft hinzufügt

--- src/components/Completed.js
+++ src/components/Completed.js
@@ -1,16 +1,37 @@
-import React from 'react'
+import React from "react";
 
-function Completed() {
-    return (
-        <div style={{minHeight:"35vh"}}>
-            <h3>COMPLETED</h3>
-            <hr/>
-            <ul className="list-group">
-                
-            </ul>
-            
+function Completed({state, dispatch}) {
+  return (
+    <div style={{ minHeight: "35vh" }}>
+      <div className="card-body">
+        <h3>COMPLETED</h3>
+        <hr />
+        <div className="d-flex flex-row flex-wrap justify-content-center">
+          {state.map((todo, index) => {
+              const onDelete=()=>{
+                dispatch({type:'delete', payload:{index:index} })
+              }
+            if (todo.completed) {
+              return (
+                <div
+                  key={index}
+                  class="card"
+                  style={{ margin: "10px", width: "300px" }}
+                >
+                  <div class="card-body">
+                    <h5 class="card-title">{index + 1}.TODO</h5>
+                    <p class="card-text">{todo.msj}</p>
+                    <button class="btn btn-success">Completed</button>
+                    <button onClick={onDelete} class="btn btn-danger mx-4">Delete</button>
+                  </div>
+                </div>
+              );
+            }
+          })}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Completed
+export default Completed;
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 
-function Todo(props) {
+function Todo({state, dispatch}) {
+ 
   return (
     <div style={{ minHeight: "40vh" }}>
       <div className="card-body">
@@ -9,20 +10,32 @@ function Todo(props) {
         <hr />
         <div className="d-flex flex-row flex-wrap justify-content-center">
           
-            {props.state.map((todo, index) => {
-              return (
+            {
+            state.map((todo, index) => {
+              const onComplate=()=>{
+                dispatch({type:'complate', payload:{index:index} })
+              }
+
+              const onDelete=()=>{
+                dispatch({type:'delete', payload:{index:index} })
+              }
+              if(todo.completed===false){
+                return (
                 
                   <div key={index} class="card" style={{ margin: "10px", width:'300px' }}>
                     <div class="card-body">
                       <h5 class="card-title">{index+1}.TODO</h5>
                       <p class="card-text">{todo.msj}</p>
-                      <button class="btn btn-primary">
-                        Completed
+                      <button onClick={onComplate} class="btn btn-primary">
+                        Incomplete
                       </button>
+                      <button onClick={onDelete} class="btn btn-danger mx-4">Delete</button>
                     </div>
                   </div>
                
               );
+              }
+              
             })}
          
         </div>
